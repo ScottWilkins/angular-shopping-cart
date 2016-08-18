cea64ed2243a4e728e296b2ab45eb20125524880
Scott Wilkins <scottwilkins@Scotts-mac.local>
2016-08-17 23:08:18 -0600

    add tea to cart

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -1,4 +1,7 @@
 app.controller('TeasController', function ($scope, TeaService) {
   $scope.view = {};
   $scope.view.teas = TeaService.all;
+  $scope.view.add = function(quantity, tea){
+    console.log(quantity + "  " + tea.price);
+  }
 })
